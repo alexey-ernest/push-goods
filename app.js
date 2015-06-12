74742d5c712fd629f53dc09f129f9f818cf5044e
Alexey <alexey.ernest@gmail.com>
2015-06-12 20:04:50 +0300

    Simple socket.io communication

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var Product = require('./lib/product');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.get('/', function (req, res, next) {
+    Product.list(function (err, products) {
+        if (err) return next(err);
+        res.render('home', { products: products });
+    });
+});
+
+app.get('/:id', function (req, res, next) {
+    Product.get(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    });
+});
+
+io.on('connection', function (socket) {
+    socket.on('product:click', function (id) {
+        console.log('Product with id ' + id + ' was clicked.');
+    });
 });
 
 module.exports = http;
